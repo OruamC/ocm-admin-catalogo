d6e58593e2a2141f8b8ea1a79bf0b6598383823f
Oruam <oru_catani@hotmail.com>
2023-11-09 14:38:01 -0300

    feat: implementing no stack trace in exceptions

--- domain/src/main/java/com/ocm/admin/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/ocm/admin/domain/exceptions/DomainException.java
@@ -4,17 +4,21 @@ import com.ocm.admin.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException{
+public class DomainException extends NoStackTraceException {
 
     private final List<Error> errors;
 
-    public DomainException(final List<Error> anErrors) {
-        super("", null, true, false);
+    public DomainException(final String aMessage, final List<Error> anErrors) {
+        super(aMessage);
         this.errors = anErrors;
     }
 
-    public static DomainException with(final List<Error> anErros) {
-        return new DomainException(anErros);
+    public static DomainException with(final Error anError) {
+        return new DomainException(anError.message(), List.of(anError));
+    }
+
+    public static DomainException with(final List<Error> anError) {
+        return new DomainException("", anError);
     }
 
     public List<Error> getErrors() {
