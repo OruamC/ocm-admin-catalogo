853c9a37346df7e92736366dc76693dce99dbb24
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2023-11-22 20:48:32 -0300

    feat: configuring spling-boot

---
+++ infrastructure/src/main/java/com/ocm/admin/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.ocm.admin.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ocm.admin")
+public class WebServerConfig {
+}
