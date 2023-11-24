7ab86a0444baa23059c3a878cc8fc8a68ae15df7
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2023-11-23 21:58:57 -0300

    feat: hikari and jpa configuration

--- infrastructure/src/test/java/com/ocm/admin/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/ocm/admin/infrastructure/MainTest.java
@@ -2,12 +2,14 @@ package com.ocm.admin.infrastructure;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
 
 public class MainTest {
 
     @Test
     public void testMain() {
-        Assertions.assertNotNull(new Main());
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
+        Assertions.assertNotNull(new Main());   
         Main.main(new String[]{});
     }
 }
