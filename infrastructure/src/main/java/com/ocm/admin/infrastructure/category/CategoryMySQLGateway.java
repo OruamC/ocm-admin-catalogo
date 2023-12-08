692032e9dc38f611324e5ed10ddf255490c54016
Oruam <oru_catani@hotmail.com>
2023-12-07 22:45:19 -0300

    test: mysql gateway annotation

---
+++ infrastructure/src/main/java/com/ocm/admin/infrastructure/category/CategoryMySQLGateway.java
@@ -0,0 +1,48 @@
+package com.ocm.admin.infrastructure.category;
+
+import com.ocm.admin.domain.category.Category;
+import com.ocm.admin.domain.category.CategoryGateway;
+import com.ocm.admin.domain.category.CategoryID;
+import com.ocm.admin.domain.category.CategorySearchQuery;
+import com.ocm.admin.domain.pagination.Pagination;
+import com.ocm.admin.infrastructure.category.persistence.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryMySQLGateway implements CategoryGateway {
+
+    private final CategoryRepository repository;
+
+    public CategoryMySQLGateway(
+            final CategoryRepository repository
+    ) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Category create(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(CategoryID anId) {
+
+    }
+
+    @Override
+    public Optional<Category> findById(CategoryID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Category update(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
+        return null;
+    }
+}
