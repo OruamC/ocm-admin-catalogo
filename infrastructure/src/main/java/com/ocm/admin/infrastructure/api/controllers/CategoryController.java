7ca2fab63586b473b1d210bbdb47a3c1f291d30d
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2024-01-03 16:44:00 -0300

    chore: openapi doc

---
+++ infrastructure/src/main/java/com/ocm/admin/infrastructure/api/controllers/CategoryController.java
@@ -0,0 +1,19 @@
+package com.ocm.admin.infrastructure.api.controllers;
+
+import com.ocm.admin.domain.pagination.Pagination;
+import com.ocm.admin.infrastructure.api.CategoryAPI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController implements CategoryAPI {
+    @Override
+    public ResponseEntity<?> createCategory() {
+        return null;
+    }
+
+    @Override
+    public Pagination<?> listCategories(String search, int page, int perPage, String sort, String direction) {
+        return null;
+    }
+}
