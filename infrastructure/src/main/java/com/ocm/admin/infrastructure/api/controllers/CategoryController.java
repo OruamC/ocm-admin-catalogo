23c8d966bc8a0640fe3c4ff646ccdc5f1346e466
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2024-01-03 17:33:23 -0300

    test: create category controller

--- infrastructure/src/main/java/com/ocm/admin/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/ocm/admin/infrastructure/api/controllers/CategoryController.java
@@ -1,15 +1,44 @@
 package com.ocm.admin.infrastructure.api.controllers;
 
+import com.ocm.admin.application.category.create.CreateCategoryCommand;
+import com.ocm.admin.application.category.create.CreateCategoryOutput;
+import com.ocm.admin.application.category.create.CreateCategoryUseCase;
 import com.ocm.admin.domain.pagination.Pagination;
+import com.ocm.admin.domain.validation.handler.Notification;
 import com.ocm.admin.infrastructure.api.CategoryAPI;
+import com.ocm.admin.infrastructure.category.models.CreateCategoryApiInput;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.util.Objects;
+import java.util.function.Function;
+
 @RestController
 public class CategoryController implements CategoryAPI {
+
+    private final CreateCategoryUseCase createCategoryUseCase;
+
+    public CategoryController(final CreateCategoryUseCase createCategoryUseCase) {
+        this.createCategoryUseCase = Objects.requireNonNull(createCategoryUseCase);
+    }
+
     @Override
-    public ResponseEntity<?> createCategory() {
-        return null;
+    public ResponseEntity<?> createCategory(final CreateCategoryApiInput input) {
+        final var aCommand = CreateCategoryCommand.with(
+                input.name(),
+                input.description(),
+                input.active() != null ? input.active() : true
+        );
+
+        final Function<Notification, ResponseEntity<?>> onError = notification ->
+                ResponseEntity.unprocessableEntity().body(notification);
+
+        final Function<CreateCategoryOutput, ResponseEntity<?>> onSuccess = output ->
+                ResponseEntity.created(URI.create("/categories/" + output.id())).body(output);
+
+        return this.createCategoryUseCase.execute(aCommand)
+                .fold(onError, onSuccess);
     }
 
     @Override
